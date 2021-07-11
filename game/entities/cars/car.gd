505d6e67de5f3c9b2ad2801c479cdc7a0928e8bd
Severi Vidnäs <severi.vidnas@gmail.com>
2021-07-11 21:19:57 +0300

    Added unit test for Car

--- game/entities/cars/car.gd
+++ game/entities/cars/car.gd
@@ -7,11 +7,11 @@ extends VehicleBody
 # Base for all Cars.
 
 # Amount of throttle input between frames.
-var _throttle_input: float = 0.0
+var _throttle_input: float = 0.0 setget set_throttle_input, get_throttle_input
 # Amount of brake input between frames.
-var _brake_input: float = 0.0
+var _brake_input: float = 0.0 setget set_brake_input, get_brake_input
 # Amount of steer input between frames.
-var _steer_input: float = 0.0
+var _steer_input: float = 0.0 setget set_steer_input, get_steer_input
 
 
 # See Node._physics_process
@@ -23,8 +23,38 @@ func _physics_process(delta: float) -> void:
   pass
 
 
+func set_throttle_input(value: float) -> void:
+  _throttle_input = value
+  pass
+
+
+func get_throttle_input() -> float:
+  return _throttle_input
+  pass
+
+
+func set_brake_input(value: float) -> void:
+  _brake_input = value
+  pass
+
+
+func get_brake_input() -> float:
+  return _brake_input
+  pass
+
+
+func set_steer_input(value: float) -> void:
+  _steer_input = value
+  pass
+
+
+func get_steer_input() -> float:
+  return _steer_input
+  pass
+
+
 # Adds throttle input and return the input value after addition.
-func _add_throttle_input(value: float) -> float:
+func add_throttle_input(value: float) -> float:
   _throttle_input = Utilities.clamp_input_unidirectional(
       _throttle_input + value
     )
@@ -33,7 +63,7 @@ func _add_throttle_input(value: float) -> float:
 
 
 # Adds brake input and return the input value after addition.
-func _add_brake_input(value: float) -> float:
+func add_brake_input(value: float) -> float:
   _brake_input = Utilities.clamp_input_unidirectional(
       _brake_input + value
     )
@@ -42,7 +72,7 @@ func _add_brake_input(value: float) -> float:
 
 
 # Adds steer input and return the input value after addition.
-func _add_steer_input(value: float) -> float:
+func add_steer_input(value: float) -> float:
   _steer_input = Utilities.clamp_input_bidirectional(
       _steer_input + value
     )
---
+++ test/game/entities/cars/car_test.gd
@@ -0,0 +1,70 @@
+###############################################################################
+# Copyright (C) 2021 Severi Vidnäs                                            #
+# Distributed under the terms of the GNU General Public License v3.0          #
+###############################################################################
+class_name CarTest
+extends GdUnitTestSuite
+# Car test suite.
+
+# TestSuite generated from
+const __source = 'res://game/entities/cars/car.gd'
+# Variable to store script reference.
+var _script: GDScript = load(__source)
+# Variable to store Car reference.
+var _node: Car = null
+
+func before() -> void:
+  _node = _script.new()
+  pass
+
+
+func after() -> void:
+  _node.free()
+  pass
+
+
+func test_add_throttle_input() -> void:
+  # Test default
+  assert_float(_node.get_throttle_input()).is_equal(0.0)
+  # Test input
+  var input: float = 0.4
+  assert_float(_node.add_throttle_input(input)).is_equal(input)
+  assert_float(_node.add_throttle_input(input)).is_equal(input * 2)
+  assert_float(_node.add_throttle_input(input)).is_less_equal(
+      Constants.UNIDIRECTIONAL_INPUT_VALUE_MAX
+    )
+  pass
+
+
+func test_add_brake_input() -> void:
+  # Test default
+  assert_float(_node.get_brake_input()).is_equal(0.0)
+  # Test input
+  var input: float = 0.4
+  assert_float(_node.add_brake_input(input)).is_equal(input)
+  assert_float(_node.add_brake_input(input)).is_equal(input * 2)
+  assert_float(_node.add_brake_input(input)).is_less_equal(
+      Constants.UNIDIRECTIONAL_INPUT_VALUE_MAX
+    )
+  pass
+
+
+func test_add_steering_input() -> void:
+  # Test default
+  assert_float(_node.get_steering_input()).is_equal(0.0)
+  # Test positive input
+  var input: float = 0.4
+  assert_float(_node.add_brake_input(input)).is_equal(input)
+  assert_float(_node.add_brake_input(input)).is_equal(input * 2)
+  assert_float(_node.add_brake_input(input)).is_less_equal(
+      Constants.BIDIRECTIONAL_INPUT_VALUE_MAX
+    )
+  # Test negative input
+  _node.set_steer_input(0.0)
+  assert_float(_node.add_brake_input(-input)).is_equal(-input)
+  assert_float(_node.add_brake_input(-input)).is_equal(-input * 2)
+  assert_float(_node.add_brake_input(-input)).is_less_equal(
+      Constants.BIDIRECTIONAL_INPUT_VALUE_MIN
+    )
+  pass
+
